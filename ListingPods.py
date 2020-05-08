e5499169adc108cf068ac3675754bc37e83fe492
Jithin Pankan <jithinpankan@Jithins-MacBook-Pro.local>
2020-05-08 19:14:20 +0530

    Modified the file by removing unwanted lines

--- ListingPods.py
+++ ListingPods.py
@@ -1,25 +1,25 @@
 from kubernetes import client, config
 from columnar import columnar
-import os
-#print(os.getcwd())
 
 config.load_kube_config()
 
 v1 = client.CoreV1Api()
-def all_pods():
+def list_all_pods():
     print("Listing pods with their IPs")
     ret = v1.list_pod_for_all_namespaces(watch=False)
-    #print ('=' *120)
-    #print (f' {"NAMESPACE":^10} {"POD IP":^20} {"HOST IP":^20} {"NODE NAME":^10} {"NAME":^15}')
-    #print ('=' *120)
+
     data = []
     headers = ['NAMESPACE','POD IP','HOST IP','NODE NAME','POD NAME']
     for i in ret.items:
         #print("%s\t\t%s\t\t%s\t\t%s\t\t%s" % (i.metadata.namespace, i.status.pod_ip,  i.status.host_ip, i.spec.node_name,  i.metadata.name ))
-
         data1 = [i.metadata.namespace, i.status.pod_ip,  i.status.host_ip, i.spec.node_name, i.metadata.name]
         data1 = data.append(data1)
-        #print(data)
         table = columnar(data,headers,no_borders=False,wrap_max=0)
+        csv = columnar(data,headers,no_borders=True,wrap_max=0)
+        fo=open("ListingPods.csv",'w')
+        fo.write(csv)
+        fo.close()
     print(table)
-all_pods()
+    #print (csv)
+
+list_all_pods()
