969b96354b3cc96ed4dae9a24db2cc20bc0d7c9a
Jithin Pankan <jithinpankan@Jithins-MacBook-Pro.local>
2020-05-11 17:06:16 +0530

    Modified the listingpods and add web app

--- ListingPods.py
+++ ListingPods.py
@@ -4,22 +4,18 @@ from columnar import columnar
 config.load_kube_config()
 
 v1 = client.CoreV1Api()
-def list_all_pods():
-    print("Listing pods with their IPs")
-    ret = v1.list_pod_for_all_namespaces(watch=False)
 
-    data = []
-    headers = ['NAMESPACE','POD IP','HOST IP','NODE NAME','POD NAME']
+def list_all_pods():
+    ret = v1.list_pod_for_all_namespaces(watch=False)
+    fo=open("ListingPods.csv",'w')
+    fo.write("NAMESPACE,POD_IP,HOST_IP,NODE_NAME,POD_NAME"+'\n')
+    fo.close()
     for i in ret.items:
         #print("%s\t\t%s\t\t%s\t\t%s\t\t%s" % (i.metadata.namespace, i.status.pod_ip,  i.status.host_ip, i.spec.node_name,  i.metadata.name ))
-        data1 = [i.metadata.namespace, i.status.pod_ip,  i.status.host_ip, i.spec.node_name, i.metadata.name]
-        data1 = data.append(data1)
-        table = columnar(data,headers,no_borders=False,wrap_max=0)
-        csv = columnar(data,headers,no_borders=True,wrap_max=0)
-        fo=open("ListingPods.csv",'w')
-        fo.write(csv)
+        data1 = ("%s,%s,%s,%s,%s" % (i.metadata.namespace, i.status.pod_ip,  i.status.host_ip, i.spec.node_name,  i.metadata.name ))
+        fo=open("ListingPods.csv",'a')
+        fo.write(data1+'\n')
         fo.close()
-    print(table)
-    #print (csv)
+
 
 list_all_pods()
