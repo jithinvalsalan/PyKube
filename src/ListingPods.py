c0c250a852dd5cefe37672e42dc9129c37b5558d
Jithin Pankan <jithinpankan@Jithins-MacBook-Pro.local>
2020-05-12 22:26:39 +0530

    Added src directory

---
+++ src/ListingPods.py
@@ -0,0 +1,20 @@
+from kubernetes import client, config
+
+config.load_kube_config()
+
+v1 = client.CoreV1Api()
+
+def list_all_pods():
+    ret = v1.list_pod_for_all_namespaces(watch=False)
+    fo=open("ListingPods.csv",'w')
+    fo.write("NAMESPACE,POD_IP,HOST_IP,NODE_NAME,POD_NAME"+'\n')
+    fo.close()
+    for i in ret.items:
+        #print("%s\t\t%s\t\t%s\t\t%s\t\t%s" % (i.metadata.namespace, i.status.pod_ip,  i.status.host_ip, i.spec.node_name,  i.metadata.name ))
+        data1 = ("%s,%s,%s,%s,%s" % (i.metadata.namespace, i.status.pod_ip,  i.status.host_ip, i.spec.node_name,  i.metadata.name ))
+        fo=open("ListingPods.csv",'a')
+        fo.write(data1+'\n')
+        fo.close()
+
+
+list_all_pods()
